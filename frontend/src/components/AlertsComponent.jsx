863fe70a6685576e56d3346483fc3f003dd2e94e
PoojaOnGit <sonawanepooja976@gmail.com>
2025-08-22 16:08:45 +0530

    final commit

---
+++ frontend/src/components/AlertsComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AlertsComponent = () => {
+  return (
+    <div>
+      <h1>Alerts Page</h1>
+      <p>Here are your alerts.</p>
+    </div>
+  );
+};
+
+export default AlertsComponent;
